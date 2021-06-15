c67fb22c05c18c4ac038d2e3de1cb8c9adf6ad0d
bllerenac <bllerenac@uni.pe>
2021-06-15 11:00:00 -0500

    add general addons default data

--- src/containers/client/reservation/components/addons_modal/generalModal.js
+++ src/containers/client/reservation/components/addons_modal/generalModal.js
@@ -1,26 +1,41 @@
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Field, formValueSelector } from 'redux-form'
 import { Button, Header, Icon, Modal } from 'semantic-ui-react'
 
-function GeneralModal (){
-  const [open, setOpen] = useState(false)
-return(
-  <Modal
-    onClose={() => setOpen(false)}
-    onOpen={() => setOpen(true)}
-    open={open}
-    trigger={<Button type='button' positive>Edit</Button>}>
+import PetCard from '../../components/petCard'
+import boardingReservationBookDetailDuck from '@reducers/client/reservation/boarding-reservation-book/detail'
 
+const selector = formValueSelector('boarding-form')
+
+function GeneralModal (props) {
+  const [open, setOpen] = useState(false)
+  const detail = useSelector(boardingReservationBookDetailDuck.selectors.detail)
+  const pets = useSelector((state) =>
+    selector(state, 'pets')
+  )
+  const selectedPets = detail.form.pet_options.filter(pet => {
+    for(let i = 0; pets.length > i ; i++){
+      if(pets[i] === pet.id){
+        return true
+      }else{
+        return false
+      }
+    }
+  })
+  console.log(detail, pets, selectedPets)
+return(
+  <Modal onClose={() => setOpen(false)} onOpen={() => setOpen(true)} open={open} trigger={<Button type='button' disabled={selectedPets.length === 0} positive>Edit</Button>}>
+    <Modal.Content>
+      {selectedPets.map( pets => <PetCard pet={pets}/> )}
+    </Modal.Content>
     <Modal.Actions>
-        <Button color='black' onClick={() => setOpen(false)}>
-          Nope
+        <Button color='red' onClick={() => setOpen(false)}>
+          Cancel
         </Button>
-        <Button
-          content="Yep, that's me"
-          labelPosition='right'
-          icon='checkmark'
-          onClick={() => setOpen(false)}
-          positive
-        />
+        <Button labelPosition='right' icon='checkmark' onClick={() => setOpen(false)} positive>
+          Save Service
+        </Button>
       </Modal.Actions>
   </Modal>
 )
